325c6a76b6b02b27ca1e23c0aa081e0383251e76
IamAnderson <omorogbeandersonsimeon@gmail.com>
2022-12-21 08:26:30 +0100

    Hans Update

--- src/data/headerData/SlideData.js
+++ src/data/headerData/SlideData.js
@@ -8,7 +8,7 @@ export const SlideData = [
         id: 1,
         img: img1,
         title: "Provision of Engineering Support",
-        text: "Geoleum Energy provides engineering support for all form of projects in the energy and non-energy sectors in Nigeria and Gulf of Guinea."
+        text: "Hans Energy provides engineering support for all form of projects in the energy and non-energy sectors in Nigeria and Gulf of Guinea."
     },
 
     {
@@ -22,13 +22,13 @@ export const SlideData = [
         id: 3,
         img: img3,
         title: "Highly trained professionals with internationally recognized certifications",
-        text: "Geoleum Energy has a community of highly trained Engineers, Scientists and Consultants with years of professional experience."
+        text: "Hans Energy has a community of highly trained Engineers, Scientists and Consultants with years of professional experience."
     },
 
     {
         id: 4,
         img: img4,
         title: "Provision of Engineering Support",
-        text: "Geoleum Energy provides engineering support for all form of projects in the energy and non-energy sectors in Nigeria and Gulf of Guinea."
+        text: "Hans Energy provides engineering support for all form of projects in the energy and non-energy sectors in Nigeria and Gulf of Guinea."
     },
 ] 
